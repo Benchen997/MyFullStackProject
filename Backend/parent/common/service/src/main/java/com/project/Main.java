5d33f3e4a8dabdb5a1a3d1e59db44de577104c2b
Benchen997 <89903375+Benchen997@users.noreply.github.com>
2024-03-13 16:39:26 +1100

    maven settings completed for backend

---
+++ Backend/parent/common/service/src/main/java/com/project/Main.java
@@ -0,0 +1,12 @@
+package com.project;
+
+/**
+ * @author Tianjia Chen
+ * Email: melbournechen0422@gmail.com
+ * @Date 2024/3/13 14:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
